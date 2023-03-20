7b514b68bd81e3d031232d357f95c919d6974d22
Jang <ahhhaaah@gmail.com>
2023-03-20 15:46:00 +0900

    feat: 기초 셋팅

---
+++ src/main/java/com/web/billim/domain/Member.java
@@ -0,0 +1,39 @@
+package com.web.billim.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "member")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // MYSQL increment_auto 로 PK 를 생성
+    @Column(name = "member_id")
+    private int member_id;
+
+    private String id;
+
+    private String password;
+
+    private String name;
+
+    private String nickname;
+
+    private String address;
+
+    private String email;
+
+    private String grade;
+
+    private Timestamp created_at;
+
+    private Timestamp updated_at;
+
+}
