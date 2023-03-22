0b922543624f1c4ff0de1f0d8e664fbd8fadb67a
yeomseohak <duatjgkr123@gmail.com>
2023-03-22 23:32:59 +0900

    feat : 회원가입 페이지 경로 설정

--- src/main/java/com/web/billim/controller/MemberController.java
+++ src/main/java/com/web/billim/controller/MemberController.java
@@ -2,8 +2,16 @@ package com.web.billim.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
+
+    @GetMapping("/memberEnroll")
+    public String memberEnroll(){
+        return "page/memberSignUp";
+    }
+
+
 }
