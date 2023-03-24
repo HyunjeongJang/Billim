81732603b2ada15cc06ede675b5bf630bcc75b1e
Jang <ahhhaaah@gmail.com>
2023-03-24 17:39:45 +0900

    feat: 레이아웃 설정

---
+++ src/main/resources/sql/insert.sql
@@ -0,0 +1,4 @@
+insert into product_category values (1 , '생활용품');
+insert into product_category values (2 , '의류잡화');
+insert into product_category values (3 , '전자기기');
+insert into product_category values (4 , '운동기구');
--- src/main/resources/static/common/default_layout.js
+++ src/main/resources/static/common/default_layout.js
@@ -1,3 +1,4 @@
+// nav 마이페이지 메뉴 접기
 $(document).ready(function () {
     $("#nav_menu ul.sub").hide();
     $("#nav_menu ul.menu_list li").click(function () {
