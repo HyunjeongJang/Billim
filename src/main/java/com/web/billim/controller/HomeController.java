a3e926a385c5a1bae2be4c49a8e7bf8efe0be2c8
yeomseohak <duatjgkr123@gmail.com>
2023-04-02 23:15:39 +0900

    feat : Spring security

--- src/main/java/com/web/billim/controller/HomeController.java
+++ src/main/java/com/web/billim/controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.web.billim.controller;
 
+import com.web.billim.dto.request.User;
+import org.springframework.boot.Banner;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model, @AuthenticationPrincipal User user) {
+        if(user != null){
+            model.addAttribute("user",user);
+        }
         return "pages/home";
     }
 }
