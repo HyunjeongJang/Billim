5c324a15319de3462a9a021d837bfc0e3a42fa8c
Jang <ahhhaaah@gmail.com>
2023-04-18 22:54:03 +0900

    feat: order script

--- src/main/java/com/web/billim/order/controller/OrderController.java
+++ src/main/java/com/web/billim/order/controller/OrderController.java
@@ -1,5 +1,8 @@
 package com.web.billim.order.controller;
 
+import com.web.billim.product.domain.Product;
+import com.web.billim.product.dto.response.ProductDetailResponse;
+import com.web.billim.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,6 +19,9 @@ import com.web.billim.order.dto.response.PaymentInfoResponse;
 import com.web.billim.order.service.OrderService;
 import com.web.billim.security.domain.User;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
@@ -23,14 +29,27 @@ public class OrderController {
 
     private final OrderService orderService;
 
+    private final ProductService productService;
+
     @GetMapping ("/order/confirm")
     public String orderConfirm(String startDate, String endDate, int productId, Model model) {
 
         model.addAttribute("startDate",startDate);
         model.addAttribute("endDate", endDate);
 
+        Product product = productService.retrieve(productId);
+        ProductDetailResponse productDetail = ProductDetailResponse.of(product);
+        List<LocalDate> alreadyDates = orderService.reservationDate(product);
+
+        model.addAttribute("product", productDetail);
+        model.addAttribute("alreadyDates",alreadyDates);
+
         return "pages/order/orderInfo";
     }
+
+
+
+
 
     @PostMapping("/order")
     public ResponseEntity<PaymentInfoResponse> order(@RequestBody OrderCommand command) {
